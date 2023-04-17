bf5c7cbff851f0c9209d3b5f229ef245ea085968
aditya-vetale <adityavetale7028@gmail.com>
2023-04-18 01:45:57 +0530

    Day 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,40 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [components, setComponenets] = useState([]);
+  const [child_sum_relation, setChild_sum_relation] = useState({});
 
   useEffect(() => {
-    console.log(components);
+    // console.log(globalTotal);
+    console.log(child_sum_relation, components);
   });
 
   function handleAddElement() {
-    setComponenets([...components, <Element key={components.length} />]);
+    setComponenets([
+      ...components,
+      <Element
+        key={components.length}
+        updateGlobalTotal={totalSum}
+        index={components.length + 1}
+      />,
+    ]);
+  }
+
+  function totalSum(value_from_child, childIndex) {
+    // child_sum_relation[childIndex] = value_from_child;
+    let tempObj = child_sum_relation;
+    console.log(tempObj);
+    tempObj[childIndex] = value_from_child;
+    setChild_sum_relation(tempObj);
   }
   return (
     <>
       {components}
       <button onClick={handleAddElement}>Add Element</button>
+      <br />
+      Total :
+      {Object.values(child_sum_relation).reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      }, 0)}
     </>
   );
 }
--- src/components/elements/app.jsx
+++ src/components/elements/app.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
 import "./style.css";
 
-function App() {
-  const [selectedElement, setSelectedElement] = useState(null);
-  const [subTypeSelected, setSubTypeSelected] = useState(null);
-
+function App(props) {
   let elements = {
     Table: { "Round Table": 700, "Square Table": 700 },
     Chair: { Regular: 300, Covered: 700 },
   };
+
+  const [selectedElement, setSelectedElement] = useState(null);
+  const [subTypeSelected, setSubTypeSelected] = useState(null);
+  const [quantity, setQuantity] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    // console.log(props.parent_total);
+  });
 
   function mainElementChanged(event) {
     setSelectedElement(event.target.value);
@@ -16,6 +23,19 @@ function App() {
 
   function subTypeChanged(event) {
     setSubTypeSelected(event.target.value);
+    setPrice(elements[selectedElement][event.target.value]);
+  }
+
+  function handleQuantity(event) {
+    setQuantity(event.target.value);
+    setTotal(event.target.value * price);
+    props.updateGlobalTotal(event.target.value * price, props.index);
+  }
+
+  function handlePrice(event) {
+    setPrice(event.target.value);
+    setTotal(quantity * event.target.value);
+    props.updateGlobalTotal(quantity * event.target.value, props.index);
   }
 
   return (
@@ -50,9 +70,13 @@ function App() {
 
       {subTypeSelected && (
         <>
-          <input type="number"></input>
-          <input type="number"></input>
-          <input type="number"></input>
+          <input type="number" value={price} onChange={handlePrice}></input>
+          <input
+            type="number"
+            onChange={handleQuantity}
+            value={quantity}
+          ></input>
+          <span>{total}</span>
         </>
       )}
     </form>
